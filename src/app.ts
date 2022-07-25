6ae19ef6ad09d48fd15a970bb458e0da9682cad6
NodjaLima <nodjamplima@gmail.com>
2022-07-25 15:46:33 -0300

    adicionando typescript e incrementando a forma de desenvolver, servidor no ar

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log('funcionei')
+import express, { Request, Response, NextFunction } from "express";
+
+const app = express();
+const porta = 3000
+
+app.listen(porta, () => {
+  console.log(`Aplicação rodando na porta ${porta}`)
+})
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({Foo:"Bar"})
+})
